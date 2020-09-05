8321f077635eb1110f985df338c9b02bd84e4568
sadisticoslution <sadisticsolutione@gmail.com>
2020-09-05 00:11:20 -0700

    Removed debug

--- assets/js/horizontal-scroll.js
+++ assets/js/horizontal-scroll.js
@@ -1,11 +1,8 @@
 (function () {
     $(document).ready(function () {
         $(".horizontal-scroll").each(function () {
-            console.log('here');
             if (!$(this).children().length)
                 return;
-
-            console.log('start');
 
             var parent = $(this),
                 child = $($(this).children()[0]);
@@ -13,8 +10,6 @@
             $(window).resize(function () {
                 var windowWidth = $(window).width(),
                     childWidth = child.width();
-
-                console.log(windowWidth, childWidth);
 
                 if (childWidth > windowWidth) {
                     parent.addClass('show');
